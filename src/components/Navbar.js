f46a60d803da51f4c86b35ea32a078e57018e7d2
ashu-tosh-singh <ashutosh.singh2780@gmail.com>
2023-05-27 22:31:54 +0530

    navbar property update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import resume from "../assets/resume.pdf";
 
 export default function Navbar() {
   return (
-    <div className=" z-50 bg-dark-500 w-full top-0 left-0 px-8 py-4 lg:px-20 xl:px-36">
+    <div className="fixed z-50 bg-dark-500 w-full top-0 left-0 px-8 py-4 lg:px-20 xl:px-36">
       <div className="flex justify-between items-center text-white">
         <img src={logo} className="App-logo w-10 " alt="logo" />
         <ul className="hidden md:flex">
